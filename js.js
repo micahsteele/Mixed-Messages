ffd07118593c6accff4b4285f5d37440b747abe4
Micah Steele <micahs66@gmail.com>
2024-10-08 11:28:04 +1100

    starsign message is logged

--- js.js
+++ js.js
@@ -1,3 +1,10 @@
 const starSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius',
     'Capricorn', 'Aquarius', 'Pisces'];
+const fortune = ['good', 'bad', 'terrible', 'awful', 'extraordinary', 'ordinary'];
+const luck = ['fortune', 'luck', 'aura', 'vibes'];
 
+function displayText() {
+    let message = `You are a ${starSign[Math.floor(Math.random() * starSign.length)]}, you will have 
+    ${fortune[Math.floor(Math.random() * fortune.length)]} ${luck[Math.floor(Math.random() * luck.length)]}`;
+    return console.log(message)
+};
